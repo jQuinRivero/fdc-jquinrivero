869920d1b06491dc07339c365ec25387642efc62
jQuinRivero <joaquinrivero94@gmail.com>
2024-04-30 20:03:52 -0300

    feat(utils): add type hints and docstrings to classes

--- src/fig_data_challenge/data_cleanser.py
+++ src/fig_data_challenge/data_cleanser.py
@@ -1,73 +1,108 @@
 import pandas as pd
 import re
 import os
+from typing import List, Dict, Optional
+from pandas import DataFrame
 
 
 class DataCleanser:
-
     def cleanse_raw_data(
-        self, df, custom_mappings=None, drop_na_columns=None, columns_to_extract=None
-    ):
-        column_mappings = {
-            col: self._map_column_names(name=col, custom_mappings=custom_mappings)
-            for col in df.columns
-        }
-        # Rename columns using the mappings
-        df = df.rename(columns=column_mappings)
+        self,
+        df: DataFrame,
+        custom_mappings: Optional[Dict[str, str]] = None,
+        drop_na_columns: Optional[List[str]] = None,
+        columns_to_extract: Optional[List[str]] = None,
+    ) -> DataFrame:
+        """
+        Cleanses the raw data according to the provided parameters.
 
-        # If drop_na_columns are provided, drop rows with missing values in those columns
-        if drop_na_columns:
-            df = self._drop_missing_values(df, drop_na_columns)
+        :param df: The DataFrame to cleanse.
+        :param custom_mappings: A dictionary of custom column name mappings.
+        :param drop_na_columns: A list of column names for which rows with missing values should be dropped.
+        :param columns_to_extract: A list of column names to extract from the DataFrame.
+        :return: The cleansed DataFrame.
+        """
+        try:
+            column_mappings = {
+                col: self._map_column_names(name=col, custom_mappings=custom_mappings)
+                for col in df.columns
+            }
+            df = df.rename(columns=column_mappings)
 
-        # If columns_to_extract are provided, extract only those columns
-        if columns_to_extract:
-            df = self._extract_columns(df, columns_to_extract)
+            if drop_na_columns:
+                updated_drop_na_columns = [
+                    column_mappings.get(col, col) for col in drop_na_columns
+                ]
+                df = self._drop_missing_values(df, updated_drop_na_columns)
 
-        return df
+            if columns_to_extract:
+                df = df[columns_to_extract]
 
-    def _map_column_names(self, name, custom_mappings=None):
-        # Check for custom mappings
-        if custom_mappings and name in custom_mappings:
-            return custom_mappings[name]
+            return df
+        except Exception as e:
+            print(f"Error occurred while cleansing data: {e}")
+            return pd.DataFrame()
 
-        # Replace '%' with 'percentage' using a regular expression
-        name = re.sub("%", "_percentage", name)
+    def _map_column_names(
+        self, name: str, custom_mappings: Optional[Dict[str, str]] = None
+    ) -> str:
+        """
+        Maps column names according to the provided custom mappings or default rules.
 
-        # Convert to lowercase and replace special characters and spaces with underscores
-        s1 = re.sub(r"([^0-9a-zA-Z_])", "_", name.lower())
+        :param name: The original column name.
+        :param custom_mappings: A dictionary of custom column name mappings.
+        :return: The mapped column name.
+        """
+        try:
+            if custom_mappings and name in custom_mappings:
+                return custom_mappings[name]
 
-        # Replace multiple occurrences of underscores with a single underscore
-        s2 = re.sub(r"_+", "_", s1)
+            name = re.sub("%", "_percentage", name)
+            s1 = re.sub(r"([^0-9a-zA-Z_])", "_", name.lower())
+            s2 = re.sub(r"_+", "_", s1)
+            s3 = re.sub(r"^[^a-zA-Z_]+", "", s2)
 
-        # Remove leading characters until we find a letter or underscore
-        s3 = re.sub(r"^[^a-zA-Z_]+", "", s2)
+            return s3
+        except Exception as e:
+            print(f"Error occurred while mapping column names: {e}")
+            return name
 
-        return s3
+    def _drop_missing_values(
+        self, df: DataFrame, drop_na_columns: List[str]
+    ) -> DataFrame:
+        """
+        Drops rows with missing values in the specified columns.
 
-    def _drop_missing_values(self, df, drop_na_columns):
-        # Drop rows with missing values in the specified columns
-        df_with_drops = df.dropna(subset=drop_na_columns)
+        :param df: The DataFrame from which to drop rows.
+        :param drop_na_columns: A list of column names for which rows with missing values should be dropped.
+        :return: The DataFrame with rows dropped.
+        """
+        try:
+            df_with_drops = df.dropna(subset=drop_na_columns)
+            rows_dropped = df.shape[0] - df_with_drops.shape[0]
 
-        # Write the rows being dropped to a log file
-        rows_dropped = df.shape[0] - df_with_drops.shape[0]
+            if rows_dropped > 0:
+                self._log_filtered_rows(rows_dropped, drop_na_columns)
 
-        if rows_dropped > 0:
-            self._log_filtered_rows(rows_dropped, drop_na_columns)
+            return df_with_drops
+        except Exception as e:
+            print(f"Error occurred while dropping missing values: {e}")
+            return df
 
-        return df_with_drops
+    def _log_filtered_rows(self, rows_dropped: int, drop_na_columns: List[str]) -> None:
+        """
+        Logs the number of rows dropped due to missing values in the specified columns.
 
-    def _extract_columns(self, df, columns_to_extract):
-        # Extract only the specified columns from the DataFrame
-
-        return df[columns_to_extract]
-
-    def _log_filtered_rows(self, rows_dropped, drop_na_columns):
-        # Write the rows being dropped to a log file
-        if os.path.exists("data/logs") == False:
-            os.makedirs("data/logs")
-        with open("data/logs/dropped_rows.log", "a") as f:
-            f.write(
-                f"Dropped {rows_dropped} rows with missing values in {drop_na_columns}\n"
-            )
-
-        return
+        :param rows_dropped: The number of rows dropped.
+        :param drop_na_columns: The columns for which rows were dropped.
+        """
+        try:
+            if not os.path.exists("data/logs"):
+                os.makedirs("data/logs")
+            with open("data/logs/dropped_rows.log", "a") as f:
+                f.write(
+                    f"Dropped {rows_dropped} rows with missing values in {drop_na_columns}\n"
+                )
+            print("File with dropped rows logged successfully! Saved to data/logs/dropped_rows.log")
+        except Exception as e:
+            print(f"Error occurred while logging filtered rows: {e}")
--- src/fig_data_challenge/data_reader.py
+++ src/fig_data_challenge/data_reader.py
@@ -1,9 +1,13 @@
-import pandas as pd
 import os
+import pandas as pd
+from typing import Any, Dict, Union
+from pandas import DataFrame
 
 
 class DataReader:
-    def read_data(self, filepath, **kwargs):
+    def read_data(
+        self, filepath: str, **kwargs: Dict[str, Any]
+    ) -> Union[DataFrame, None]:
         """
         Reads data from a file based on the extension and returns a pandas DataFrame.
 
@@ -13,14 +17,14 @@ class DataReader:
         """
         # Determine the file extension
         _, extension = os.path.splitext(filepath)
-        extension = extension.lower()
+        extension = extension.lower() if extension else extension
 
         # Select the appropriate pandas read function based on file extension
         if extension == ".csv":
             return pd.read_csv(filepath, **kwargs)
         elif extension in [".xls", ".xlsx"]:
             return pd.read_excel(filepath, **kwargs)
-        # Add additional conditions for other file types if necessary
+
         else:
             raise NotImplementedError(
                 f"Reading data from files with extension '{extension}' is not supported."
